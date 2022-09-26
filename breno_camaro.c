28bd6a556b6ebb2efca4c1bd405ae0ef65c2966a
Breno Camarô <111977366+BrenoCamaro@users.noreply.github.com>
2022-09-26 19:34:51 -0300

    Add files via upload
    Esse programa tem a função de informar ao usuário se ele pode votar, se não pode votar, se seu voto é opcional ou se ele ganhará um prêmio com base na idade informada por ele. 
    
    Obs.: Esse programa não considerou as reais legislações com relação ao voto no Brasil. É apenas uma atividade para exercitar o conceito de condicionais aprendido em sala de aula.

--- breno_camaro.c
+++ breno_camaro.c
@@ -1,62 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main() {
-system("color 7c");
-
-char nome[40];
-int idade;
-char sexo [10];
-float altura;
-char endereco [50];
-int numero_endereco;
-char data_nascimento [15];
-char cpf [50];
-
-
-printf("Digite seu nome:");
-scanf("%[^\n]s",nome);
-fflush(stdin);
-
-printf("Digite sua idade:");
-scanf("%d", &idade);
-fflush(stdin);
-
-printf("Digite seu sexo:");
-scanf(" %s", sexo);
-fflush(stdin);
-
-printf("Digite sua altura:");
-scanf("%f", &altura);
-fflush(stdin);
-
-printf("Digite seu endereco:");
-scanf ("%[^\n]s", endereco);
-fflush(stdin);
-
-printf("Digite o numero do seu endereco:");
-scanf("%d", &numero_endereco);
-fflush(stdin);
-
-printf("Digite sua data de nascimento:");
-scanf("%s", data_nascimento);
-fflush(stdin);
-
-printf("Digite seu cpf:");
-scanf("%s", cpf);
-fflush(stdin);
-
-printf("+ - - - - - - - - - - - - - - - - - - - - - - - +\n\n");
-printf("|              DADOS INFORMADOS\n\n");
-printf("+ - - - - - - - - - - - - - - - - - - - - - - - +\n\n");
-printf("\nNome: %s\n\nIdade: %d\n\nSexo: %s\n\nAltura: %.2f\n\nEndereco: %s\n\nNumero do endereco: %d\n\nData de nascimento: %s\n\nCPF: %s", nome,idade,sexo,altura,endereco,numero_endereco,data_nascimento,cpf);
-
-
-
-
-     
-  
-
+	
+		int idade=0;
+		
+		printf("Informe a sua idade:");
+		scanf("%d", & idade);
+		
+		if(idade<0){
+			printf("Nao nasceu!");
+		}
+			else{
+				if(idade<16){
+					printf("Nao vota!");
+				}
+					else{
+						if(idade<18){
+							printf("Voto opcional!");
+						}
+							else{
+								if(idade<65){
+									if(idade==41){
+										printf("Nao vota e ganha premio!");
+									}else{
+										printf("Voto obrigatorio!");
+									}
+									
+									
+								}
+									else{
+										if(idade>=65){
+											if(idade==88){
+												printf("Nao vota e ganha premio!");
+											}else{
+												printf("Voto opcional!");
+											}
+											
+										}
+										
+									}
+							}
+					}
+					
+				
+			}
+			
+				
+			
+			
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	return 0;
-
 }
